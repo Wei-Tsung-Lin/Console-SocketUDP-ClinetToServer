154e53e9edb6a8be5b4e2a17b946215d27a3d515
Wei-Tsung Lin <72340341+Wei-Tsung-Lin@users.noreply.github.com>
2022-03-11 22:07:49 +0800

    Add files via upload

---
+++ MainProgram.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class MainProgram
+    {
+
+        static void ReceivedMessage(IPEndPoint ip, UdpClient udpClient)
+        {
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(udpClient.Receive(ref ip)));
+        }
+
+        static void Main()
+        {
+ 
+            Console.WriteLine("This is Server...\n");
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 5555);
+
+            UdpClient udpclient = new UdpClient(ip.Port);
+
+            while (true)
+            {
+                ReceivedMessage(ip,udpclient);
+            }
+
+        }
+    }
+}
